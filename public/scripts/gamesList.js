dccb8b6a3110697b076e1a17de7f77ac618ce480
Joe Duncko <joe@drund.com>
2017-04-01 04:24:05 -0400

    Loading gamesList data from server

--- public/scripts/gamesList.js
+++ public/scripts/gamesList.js
@@ -7,40 +7,20 @@ class GamesList extends React.Component { // eslint-disable-line no-unused-vars
     }
 
     componentDidMount() {
-        // fetch('/game/list')
-        //     .then(function(response) {
-        //         console.log(response);
-        //         console.log(response.json());
-        //         // return response.json()
-        //     });
-
-        // fetch('/game/list')
-        //     .then(response => {
-        //         const reader = response.body.getReader();
-        //
-        //         reader.read().then(function process(result) {
-        //             console.log(result);
-        //             if (result.done) {
-        //                 return;
-        //             }
-        //             console.log(`Received a ${result.value.length} byte chunk of data`);
-        //             return reader.read().then(process);
-        //         }).then(() => {
-        //             console.log('All done!');
-        //         });
-        //     });
-
-        // fetch('//localhost:3000/game/list', {
-        //     method: 'GET'
-        // })
-        //     .then((response) => {
-        //         return response.json();
-        //     })
-        //     .then(response => {
-        //         this.setState({
-        //             games: response
-        //         });
-        //     });
+        fetch('/game/list', {
+            credentials: 'include'
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then(response => {
+                this.setState({
+                    games: response
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
     }
 
     render() {
