11d01e10b0b565b23eb553b55a316198b04e0a01
Joe Duncko <joe@drund.com>
2017-04-15 18:08:21 -0400

    Gameloop works!

--- gameloop.js
+++ gameloop.js
@@ -1,99 +1,125 @@
 //schedule to update all games every minute on the 0th second
 var schedule = require('node-schedule');
 var Game = require('./models/game');
-var PF = require('pathfinding');
+var Pathfinding = require('pathfinding');
 
 var enemyCloseRange = require('./entities/enemies/enemy_close_range');
 var modeSurvival = require('./entities/modes/mode_survival');
 
-console.log('init game loop')
+console.log('init game loop');
 
 var gameloop = schedule.scheduleJob('0 * * * * *', function(){
-    console.log("scheduled job is running")
+    console.log('scheduled job is running');
 
-    Game.getActiveGames(function(err, games){
-        var activeGames = games;
-
+    Game.getActiveGames(function(err, activeGames){
         //for all active games
         for (var i = 0; i < activeGames.length; i++){
-            //mark them as done if their base is dead
-            if (activeGames[i].baseHealth === 0){
-                markGameAsFinished(activeGames[i]._id);
-            } else{
-                //otherwise move the enemies that need moved
-                for(var k = 0; k < activeGames[i].enemies.length; k++){
-                    moveEnemyTowardsCenter(activeGames[i].enemies[k]);
+            //create a grid to represent this game's grid
+            var grid = new Pathfinding.Grid(modeSurvival.mapSize, modeSurvival.mapSize);
+            //initialize finder algorithm
+            var finder = new Pathfinding.AStarFinder();
+
+            //for each enemy, populate the grid with enemy locations - mark them as "unwalkable"
+            //TODO: do this for towers too
+            for(var j = 0; j < activeGames[i].enemies.length; j++){
+                console.log('set up grid', activeGames[i].enemies[j].positionX, activeGames[i].enemies[j].positionY);
+                grid.setWalkableAt(activeGames[i].enemies[j].positionX, activeGames[i].enemies[j].positionY, false);
+            }
+
+            //for each enemy
+            for(var k = 0; k < activeGames[i].enemies.length; k++){
+                //figure out that enemy's path to the center
+                var path = finder.findPath(activeGames[i].enemies[k].positionX, activeGames[i].enemies[k].positionY, 3, 3, grid);
+                //note: path[0] is your current position, so path[1] is the next positon
+
+                if(path.length === 0){
+                    // if there is no way to move to get to the center, don't do anything
+                    // intentionally left empty
+                    console.log('path length is 0!')
+                } else if(path[1] === [2][2]){
+                    // else, if the enemy is already moved within range of a target
+                    // (currently only the main building, hard coded at [2][2]),
+                    // have it attack said target
+                    activeGames[i].baseHealth = activeGames[i].baseHealth - enemyCloseRange.damage;
+                    console.log('attacked main building!')
+                } else {
+                    // else, the enemy should move toward the target
+                    console.log('should move to', path[1][0], path[1][1]);
+
+                    //remove the enemy from the grid position it is currently at on the grid, making it walkable again
+                    grid.setWalkableAt(activeGames[i].enemies[k].positionX, activeGames[i].enemies[k].positionY, true);
+                    //make the grid position the enemy is going to unwalkable
+                    grid.setWalkableAt(path[1][0], path[1][1], false);
+
+                    //make the change to the enemy's position in our array
+                    activeGames[i].enemies[k].positionX = path[1][0];
+                    activeGames[i].enemies[k].positionY = path[1][1];
+
+                    console.log('new position', activeGames[i].enemies[k].positionX, activeGames[i].enemies[k].positionY)
                 }
-                //then add a new enemy to the board
-                addNewEnemy(activeGames[i]._id);
             }
-        }
 
+            //mark the game as done if their base is dead
+            if (activeGames[i].baseHealth === 0){
+                activeGames[i].isGameOver = true;
+            } else{
+                // If the game isn't done
+                // then add a new enemy to the board
+
+
+                //choose x and y where enemy should spawn
+                //currently only spawning in corners for no good reason
+                var positionX;
+                var positionY;
+
+                if(Math.random() >= 0.5){
+                    positionX = 1;
+                } else{
+                    positionX = modeSurvival.mapSize - 1;
+                }
+
+                if(Math.random() >= 0.5){
+                    positionY = 1;
+                } else{
+                    positionY = modeSurvival.mapSize - 1;
+                }
+
+                //make sure it isn't where an enemy is already
+                if(grid.nodes[positionX][positionY].walkable){
+                    // if it isn't where an enemy is already, spawn it
+
+                    //defaulting enemy to enemyCloseRange for now
+                    //can probably do this cleaner with the "new" keyword?
+                    activeGames[i].enemies.push({
+                        type: enemyCloseRange.name,
+                        health: enemyCloseRange.maxHealth,
+                        positionX: positionX,
+                        positionY: positionY
+                    });
+                } else{
+                    // if it is where an enemy is already, just don't spawn anything, becuase we are lazy
+                }
+
+                //Add resources to your pool, based on your game type
+                activeGames[i].resources += modeSurvival.resourcesPerRound;
+
+                //iterate round number
+                activeGames[i].round += 1;
+            }
+
+            activeGames[i].save(function (err, updatedGame){
+                if (err) {
+                    console.log(err);
+                } else{
+                    console.log('updated game', updatedGame);
+                }
+            });
+        }
     });
+    
+    //Now that the game loop has run, send a socket.io message to all clients with their updated info
+    //TODO
 });
-
-var markGameAsFinished = function(gameId){
-    Game.getGameById(gameId, function (err, game){
-        if (err) {
-            console.log(err);
-        }
-
-        game[0].isGameOver = true;
-
-        game[0].save(function (err, updatedGame){
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
-};
-
-var moveEnemyTowardsCenter = function(enemy){
-    // console.log('should move enemy here');
-    //use https://www.npmjs.com/package/pathfinding, already included above
-};
-
-var addNewEnemy = function(gameId){
-    //choose x and y where enemy should spawn
-    //currently only spawning in corners for no good reason
-    var positionX;
-    var positionY;
-
-    if(Math.random() >= 0.5){
-        positionX = 1;
-    } else{
-        positionX = modeSurvival.mapSize;
-    }
-
-    if(Math.random() >= 0.5){
-        positionY = 1;
-    } else{
-        positionY = modeSurvival.mapSize;
-    }
-
-    Game.getGameById(gameId, function (err, game){
-        if (err) {
-            console.log(err);
-        }
-
-        //defaulting enemy to enemyCloseRange for now
-        game[0].enemies.push({
-            type: enemyCloseRange.name,
-            health: enemyCloseRange.maxHealth,
-            positionX: positionX,
-            positionY: positionY
-        });
-
-        //iterate round number
-        game[0].round += 1;
-
-        game[0].save(function (err, updatedGame){
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
-};
 
 // make this available to our users in our Node applications
 module.exports = gameloop;
